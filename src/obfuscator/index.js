40edd633d0c38262574cdfc58e8beb650e663887
Godx2nyx <ififa1504@gmail.com>
2026-09-13 12:03:18 +0700

    Create index.js

---
+++ src/obfuscator/index.js
@@ -0,0 +1,40 @@
+export {
+  Obfuscator,
+  obfuscate,
+  process,
+  compileVM,
+  transform,
+  createObfuscator,
+  WATERMARK,
+  CHUNK_NAME,
+  VM_VERSION,
+  ARCHITECTURE,
+  normalizeOptions
+} from "./obfuscator.js"
+
+import Obfuscator, {
+  obfuscate,
+  process,
+  compileVM,
+  transform,
+  createObfuscator,
+  WATERMARK,
+  CHUNK_NAME,
+  VM_VERSION,
+  ARCHITECTURE,
+  normalizeOptions
+} from "./obfuscator.js"
+
+export default {
+  Obfuscator,
+  obfuscate,
+  process,
+  compileVM,
+  transform,
+  createObfuscator,
+  WATERMARK,
+  CHUNK_NAME,
+  VM_VERSION,
+  ARCHITECTURE,
+  normalizeOptions
+}
