7d63a2d4670068a288fb4bdf0cc3151fc3e6ca05
Godx2nyx <ififa1504@gmail.com>
2026-09-13 12:11:18 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,27 @@
 export * from "./random.js"
 export * from "./hash.js"
+export * from "./encoding.js"
+export * from "./crypto.js"
+export * from "./bytes.js"
 
 import Random from "./random.js"
 import hash from "./hash.js"
+import encoding from "./encoding.js"
+import crypto from "./crypto.js"
+import bytes from "./bytes.js"
 
 export {
   Random,
-  hash
+  hash,
+  encoding,
+  crypto,
+  bytes
 }
 
 export default {
   Random,
-  ...hash
+  ...hash,
+  ...encoding,
+  ...crypto,
+  ...bytes
 }
